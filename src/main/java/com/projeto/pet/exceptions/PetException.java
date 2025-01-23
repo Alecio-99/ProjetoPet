0cb3383aba55e4b0569dfb4318eae9e7c84c5b92
Alecio <aleciosilveira97@gmail.com>
2025-01-23 01:19:35 -0300

    Configurando Exceptions de trocas de senha

---
+++ src/main/java/com/projeto/pet/exceptions/PetException.java
@@ -0,0 +1,21 @@
+package com.projeto.pet.exceptions;
+
+public class PetException extends RuntimeException{
+
+    private PetException (String message) {
+        super(message);
+    }
+
+    public static PetException senhaAtualIncorreta() {
+        return new PetException("Senha atual incorreta");
+    }
+
+    public static PetException novaSenhaNaoConfere(){
+        return new PetException("As novas senhas não conferem");
+    }
+
+    public static PetException usuarioNaoEncontrado(Long id){
+        return new PetException("Usuário com ID " + id + " não encontrado");
+    }
+}
+
