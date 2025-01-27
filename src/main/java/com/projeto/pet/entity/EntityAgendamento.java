d469540db23158f6f357b3473c3bb1269a730648
Alecio <aleciosilveira97@gmail.com>
2025-01-27 00:31:07 -0300

    Criação dos enuns de status e ajuste na entidade de agendamento

--- src/main/java/com/projeto/pet/entity/EntityAgendamento.java
+++ src/main/java/com/projeto/pet/entity/EntityAgendamento.java
@@ -2,11 +2,14 @@ package com.projeto.pet.entity;
 
 import com.projeto.pet.enuns.EnumBaia;
 import com.projeto.pet.enuns.EnumPorteDog;
+import com.projeto.pet.enuns.EnumStatusDog;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -21,11 +24,14 @@ public class EntityAgendamento {
     private long id;
     private String nameDog;
     private String raca;
-    private String agenda;
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
     @Enumerated(EnumType.STRING)
     private EnumPorteDog porteDog;
     @Enumerated(EnumType.STRING)
     private EnumBaia baia;
+    @Enumerated(EnumType.STRING)
+    private EnumStatusDog statusDog;
     @ManyToOne
     @JoinColumn(name = "id_dono")
     private EntityCadastroPet dono;
--- src/main/java/com/projeto/pet/enuns/EnumStatusDog.java
+++ src/main/java/com/projeto/pet/enuns/EnumStatusDog.java
@@ -1,4 +1,47 @@
 package com.projeto.pet.enuns;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EnumStatusDog {
+
+    PENDENTE ("P", "Pendente"),
+    AGENDADO ("A", "Agendado"),
+    REALIZADO ("R", "Realizado"),
+    CANCELADO ("C", "Cancelado"),
+    EM_ANDAMENTO ("E", "Em andamento");
+
+    private String codigo;;
+    private String descricao;
+
+    private EnumStatusDog(String codigo, String descricao){
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    @JsonValue
+    public String getCodigo(){ return  codigo; }
+
+    public void setCodigo(String codigo){this.codigo = codigo; }
+
+    public String getDescricao(){ return  descricao; }
+
+    public void setDescricao(String descricao){ this.descricao = descricao; }
+
+
+    public static EnumStatusDog valor (String codigo) {
+
+        if(codigo.equals("P")){
+            return PENDENTE;
+        }else if(codigo.equals("A")){
+            return AGENDADO;
+        }else if(codigo.equals("R")){
+            return REALIZADO;
+        }else if(codigo.equals("C")){
+            return CANCELADO;
+        }else if(codigo.equals("E")){
+            return EM_ANDAMENTO;
+        }else{
+            return null;
+        }
+    }
 }
